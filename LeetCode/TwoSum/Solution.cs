3d2dbbdbadf24a73c533104df604e42b67b1b042
Philippe Côté <cote.philippe@hotmail.com>
2021-08-12 11:35:05 -0400

    Solved TwoSum. Time: 252 ms (>63.02%). Space: 32.9 MB (<17.31%).

--- LeetCode/TwoSum/Solution.cs
+++ LeetCode/TwoSum/Solution.cs
@@ -1,12 +1,24 @@
-﻿namespace LeetCode
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
 {
     namespace TwoSum
     {
         public class Solution
         {
+            // Runtime: 252 ms, faster than 63.02% of C# online submissions for Two Sum.
+            // Memory Usage: 32.9 MB, less than 17.31% of C# online submissions for Two Sum.
             public int[] TwoSum(int[] nums, int target)
             {
-                return new int[0];
+                Dictionary<int, int> complements = new Dictionary<int, int>();
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (complements.ContainsKey(nums[i]))
+                        return new int[] { complements[nums[i]], i };
+                    complements.Add(target - nums[i], i);
+                }
+                throw new ArgumentException("No valid sum exist");
             }
         }
     }
--- Tests/TwoSumTest.cs
+++ Tests/TwoSumTest.cs
@@ -1,14 +1,32 @@
 using System;
+using LeetCode.TwoSum;
 using Xunit;
 
 namespace Tests
 {
     public class TwoSumTest
     {
-        [Fact]
-        public void Test()
-        {
+        Solution solution = new Solution();
 
+        [Fact]
+        public void Test_Example1()
+        {
+            int[] result = solution.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            Assert.Equal(new int[] { 0, 1 }, result);
+        }
+
+        [Fact]
+        public void Test_Example2()
+        {
+            int[] result = solution.TwoSum(new int[] { 3, 2, 4 }, 6);
+            Assert.Equal(new int[] { 1, 2 }, result);
+        }
+
+        [Fact]
+        public void Test_Example3()
+        {
+            int[] result = solution.TwoSum(new int[] { 3, 3 }, 6);
+            Assert.Equal(new int[] { 0, 1 }, result);
         }
     }
 }
