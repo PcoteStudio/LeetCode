0df645a9153cddb922a1f0c42c0c6e3ceb9c1890
Philippe Côté <cote.philippe@hotmail.com>
2021-08-12 11:02:41 -0400

    Set up solution and tests.

---
+++ LeetCode/TwoSum/Solution.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode
+{
+    namespace TwoSum
+    {
+        public class Solution
+        {
+            public int[] TwoSum(int[] nums, int target)
+            {
+                return new int[0];
+            }
+        }
+    }
+}
---
+++ Tests/TwoSumTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class TwoSumTest
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
