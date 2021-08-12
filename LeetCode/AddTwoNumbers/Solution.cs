96adba1cd8a17cdac8e32bde8697d29e5640cfbb
Philippe Côté <cote.philippe@hotmail.com>
2021-08-12 13:32:40 -0400

    Solved AddTwoNumbers. Time: 100 ms (<91.01%). Space: 28.6 MB (<9.14%).

---
+++ LeetCode/AddTwoNumbers/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+
+namespace LeetCode
+{
+    namespace AddTwoNumbers
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public class Solution
+        {
+            // Runtime: 100 ms, faster than 91.01% of C# online submissions for Add Two Numbers.
+            // Memory Usage: 28.6 MB, less than 9.14% of C# online submissions for Add Two Numbers.
+            public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+            {
+                BigInteger total = GetNumber(l1) + GetNumber(l2);
+                return GetNode(total) ?? new ListNode(0, null);
+            }
+
+            public BigInteger GetNumber(ListNode node)
+            {
+                if (node == null)
+                    return 0;
+                return GetNumber(node.next) * 10 + node.val;
+            }
+
+            public ListNode GetNode(BigInteger value)
+            {
+                if (value == 0)
+                    return null;
+                return new ListNode((int)(value % 10), GetNode(value / 10));
+            }
+        }
+    }
+}
---
+++ Tests/AddTwoNumbersTest.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Numerics;
+using LeetCode.AddTwoNumbers;
+using Xunit;
+
+namespace Tests
+{
+    public class AddTwoNumbersTest
+    {
+        Solution solution = new Solution();
+
+        [Fact]
+        public void Test_SameDepth()
+        {
+            ListNode l1 = new ListNode(3, null);
+            l1 = new ListNode(4, l1);
+            l1 = new ListNode(2, l1);
+
+            ListNode l2 = new ListNode(4, null);
+            l2 = new ListNode(6, l2);
+            l2 = new ListNode(5, l2);
+
+            ListNode result = solution.AddTwoNumbers(l1, l2);
+            Assert.Equal(new BigInteger(807), solution.GetNumber(result));
+        }
+
+        [Fact]
+        public void Test_AddZero()
+        {
+            ListNode l1 = new ListNode(0, null);
+
+            ListNode l2 = new ListNode(0, null);
+
+            ListNode result = solution.AddTwoNumbers(l1, l2);
+            Assert.Equal(new BigInteger(0), result.val);
+            Assert.Null(result.next);
+        }
+
+        [Fact]
+        public void Test_DifferentDepth()
+        {
+            ListNode l1 = new ListNode(9, null);
+            l1 = new ListNode(9, l1);
+            l1 = new ListNode(9, l1);
+            l1 = new ListNode(9, l1);
+            l1 = new ListNode(9, l1);
+            l1 = new ListNode(9, l1);
+            l1 = new ListNode(9, l1);
+
+            ListNode l2 = new ListNode(9, null);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+
+            ListNode result = solution.AddTwoNumbers(l1, l2);
+            Assert.Equal(new BigInteger(10009998), solution.GetNumber(result));
+        }
+
+        [Fact]
+        public void Test_NumberLargerThanLong()
+        {
+            ListNode l1 = new ListNode(9, null);
+
+            ListNode l2 = new ListNode(9, null);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(9, l2);
+            l2 = new ListNode(1, l2);
+
+            ListNode result = solution.AddTwoNumbers(l1, l2);
+            Assert.Equal(BigInteger.Parse("10000000000000000000000000"), solution.GetNumber(result));
+        }
+
+        [Fact]
+        public void Test_GetNumber()
+        {
+            ListNode list = new ListNode(4, null);
+            list = new ListNode(6, list);
+            list = new ListNode(5, list);
+
+            Assert.Equal(new BigInteger(465), solution.GetNumber(list));
+        }
+
+        [Fact]
+        public void Test_GetNode()
+        {
+            ListNode node = solution.GetNode(301);
+            Assert.Equal(1, node.val);
+            Assert.Equal(0, node.next.val);
+            Assert.Equal(3, node.next.next.val);
+            Assert.Null(node.next.next.next);
+        }
+    }
+}
